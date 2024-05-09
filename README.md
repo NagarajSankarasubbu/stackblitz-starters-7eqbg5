3395631ba7f7a981ce49405a2d56b1744bb9addb
NagarajSankarasubbu <169047128+NagarajSankarasubbu@users.noreply.github.com>
2024-05-09 17:37:06 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stackblitz-starters-7eqbg5
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/NagarajSankarasubbu/stackblitz-starters-7eqbg5)
---
+++ src/app.component.ts
@@ -0,0 +1,20 @@
+import {Component} from '@angular/core';
+
+/**
+ * @title Testing with MatBadgeHarness
+ */
+ @Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: []
+})
+
+export class AppComponent {
+}
+
+
+/**  Copyright 2024 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at https://angular.io/license */
